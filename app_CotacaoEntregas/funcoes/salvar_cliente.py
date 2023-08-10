15d2e89efe4980be341229dd45e4e3efd12296f6
Brunobh51 <89748258+Brunobh51@users.noreply.github.com>
2023-08-10 12:47:24 -0300

    teste

--- app_CotacaoEntregas/funcoes/salvar_cliente.py
+++ app_CotacaoEntregas/funcoes/salvar_cliente.py
@@ -1,17 +1,31 @@
-from django.db import models
-from models import Cliente
+def criar_cliente(request):
+    valorboy = request.POST.get('valormotoboy')
+    nome = request.POST.get('nome_cliente')
+    telefone = request.POST.get('telefone')
+    cep = request.POST.get('cep_cliente')
+    rua = request.POST.get('rua_cliente')
+    bairro = request.POST.get('bairro_cliente')
+    cidade = request.POST.get('cidade_cliente')
+    numero = request.POST.get('numero_cliente')
+    complemento = request.POST.get('complemento_cliente')
+    valor_pedido = request.POST.get('valor_pedido')
+    informacoes_adicionais = request.POST.get(
+        'exampleFormControlTextarea1')
+    pedido_pago = request.POST.get('pedido_pago')
+    motoboy_pago = request.POST.get('motoboy_pago')
 
-
-def salvar_dados_cliente(nome, telefone, endereco, valor_pedido, valor_boy, pedido_pago, motoboy_pago, descricao):
-    cliente = Cliente(
+    # Crie uma instância do modelo Cliente
+    novo_cliente = Cliente(
         nome=nome,
         telefone=telefone,
-        endereco=endereco,
+        endereco=f'{cep}, {rua}, {bairro}, {cidade}, {numero}',
         valor_pedido=valor_pedido,
-        valor_boy=valor_boy,
+        valor_boy=valorboy,
+        descricao=informacoes_adicionais,
         pedido_pago=pedido_pago,
         motoboy_pago=motoboy_pago,
-        descricao=descricao
     )
-    cliente.save()
-    return ('ok')
+
+    # Salve o cliente no banco de dados
+    novo_cliente.save()
+    print(motoboy_pago)
