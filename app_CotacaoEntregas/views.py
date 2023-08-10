476e1ef143bdc9d95693a7420f10b1fb57310646
Brunobh51 <89748258+Brunobh51@users.noreply.github.com>
2023-08-10 12:47:14 -0300

    s

--- app_CotacaoEntregas/views.py
+++ app_CotacaoEntregas/views.py
@@ -48,38 +48,3 @@ def cotacao(request):
         return render(request, 'app_CotacaoEntregas/cotacao.html', context)
     else:
         return render(request, 'app_CotacaoEntregas/cotacao.html')
-
-
-def criar_cliente(request):
-    if request.method == "POST":
-        valorboy = request.POST.get('valormotoboy')
-        nome = request.POST.get('nome_cliente')
-        telefone = request.POST.get('telefone')
-        cep = request.POST.get('cep_cliente')
-        rua = request.POST.get('rua_cliente')
-        bairro = request.POST.get('bairro_cliente')
-        cidade = request.POST.get('cidade_cliente')
-        numero = request.POST.get('numero_cliente')
-        complemento = request.POST.get('complemento_cliente')
-        valor_pedido = request.POST.get('valor_pedido')
-        informacoes_adicionais = request.POST.get(
-            'exampleFormControlTextarea1')
-        pedido_pago = request.POST.get('pedido_pago')
-        motoboy_pago = request.POST.get('motoboy_pago')
-
-        # Crie uma instância do modelo Cliente
-        novo_cliente = Cliente(
-            nome=nome,
-            telefone=telefone,
-            endereco=f'{cep}, {rua}, {bairro}, {cidade}, {numero}',
-            valor_pedido=valor_pedido,
-            valor_boy=valorboy,
-            descricao=informacoes_adicionais,
-            pedido_pago=pedido_pago,
-            motoboy_pago=motoboy_pago,
-        )
-
-        # Salve o cliente no banco de dados
-        novo_cliente.save()
-        print(motoboy_pago)
-        
