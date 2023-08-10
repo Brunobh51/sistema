6f0e71337cd990b1c2a46f57e79dcb7e9a8bc35b
Brunobh51 <89748258+Brunobh51@users.noreply.github.com>
2023-08-10 12:32:21 -0300

    a

--- app_CotacaoEntregas/urls.py
+++ app_CotacaoEntregas/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.dashboard, name='dashboard'),
     path("cotacao/", views.cotacao, name='cotacao'),
+    path("criar_cliente/", views.criar_cliente, name='criar_cliente')
     # Outras URLs relacionadas à cotação de entregas podem ser adicionadas aqui
 ]
